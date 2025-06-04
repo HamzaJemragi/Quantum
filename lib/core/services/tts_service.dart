7fe37556ad8a97a46e66989067f1cdb0d755d5d2
Yassine <outmoulayteyassine@gmail.com>
2025-06-04 14:45:21 +0000

    push main changes

--- lib/core/services/tts_service.dart
+++ lib/core/services/tts_service.dart
@@ -3,21 +3,30 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
 class TtsService {
-  final FlutterTts _flutterTts = FlutterTts();
+  final String apiKey = '5a6ba62655984add82c2e84bda3c35ec'; // Put your real key here
 
-  Future<String> speakToFile(String text, String langCode) async {
-    // Définir la langue
-    await _flutterTts.setLanguage(langCode);
-    await _flutterTts.setSpeechRate(0.5);
+  Future<File?> getTtsFile(String text, {String lang = 'fr-fr'}) async {
+    final url = Uri.parse(
+      'https://api.voicerss.org/?key=$apiKey&hl=$lang&src=${Uri.encodeComponent(text)}&c=MP3&f=44khz_16bit_stereo',
+    );
 
-    // Créer un chemin de fichier temporaire
-    final dir = await getTemporaryDirectory();
-    final filePath = "${dir.path}/${const Uuid().v4()}.wav";
+    final response = await http.get(url);
 
-    // Générer l'audio
-    await _flutterTts.synthesizeToFile(text, filePath);
-
-    return filePath;
+    if (response.statusCode == 200) {
+      final dir = await getTemporaryDirectory();
+      final filePath = '${dir.path}/tts_${DateTime.now().millisecondsSinceEpoch}.mp3';
+      final file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+      return file;
+    } else {
+      print('TTS Error: ${response.statusCode} ${response.body}');
+      return null;
+    }
   }
 }
+
